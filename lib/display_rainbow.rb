9a4b4df090f6a4a4f2882a9b484d9b15c4e7d216
summer trainer <summertrainer@yahoo.com>
2021-06-12 16:37:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(colors)
 puts colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-puts "B: #colors[5], G: #colors[3], I: #colors[5], O: #colors[1], R: #colors[0], V: #colors[6], Y: #colors[2]"
+
 end 
